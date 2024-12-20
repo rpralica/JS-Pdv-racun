5e31b90cfc8b17879352b64badf37a6a3bff7b4a
rpralica <rpralica@gmail.com>
2024-12-20 16:16:06 +0100

    Fixed errors

--- script.js
+++ script.js
@@ -12,16 +12,45 @@ const tdSaPdv = document.getElementById('tdSaPdv');
 const tdIznosSaPdv = document.getElementById('tdIznosSaPdv');
 
 function oduzmiPdv() {
-  const pdv = inpOduzmipdvVrijednost.value / (1 + minusPdvPostotak.value / 100);
+  const vrijednost = parseFloat(inpOduzmipdvVrijednost.value);
+  const procenat = parseFloat(minusPdvPostotak.value);
+
+  if (isNaN(vrijednost) || isNaN(procenat)) {
+   Swal.fire({
+     icon: 'error',
+     title: 'Pogrešan unos!!',
+     text: 'Molim unesite brojčane vrijednosti!',
+   });
+    inpOduzmipdvVrijednost.value = '';
+    minusPdvPostotak.value = '17';
+    return;
+  }
+
+  const pdv = vrijednost / (1 + procenat / 100);
   tdBezPdv.textContent = pdv.toFixed(2);
-  tdIznosBezpdv.textContent = (inpOduzmipdvVrijednost.value - pdv).toFixed(2);
+  tdIznosBezpdv.textContent = (vrijednost - pdv).toFixed(2);
 }
 
 btnOduzmipdv.addEventListener('click', oduzmiPdv);
 
 function dodajPdv() {
-  const pdv = inpDodajpdvVrijednost.value * (plusPdvPostotak.value / 100);
-  tdIznosSaPdv.textContent = pdv.toFixed(2);
+
+  const vrijednost= parseFloat(inpDodajpdvVrijednost.value);
+  const procenat=parseFloat(plusPdvPostotak.value); 
+  if (isNaN(vrijednost) || isNaN(procenat)) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Pogrešan unos!!',
+      text: 'Molim unesite brojčane vrijednosti!',
+    });
+    
+    const pdv =vrijednost * (procenat/ 100);
+    tdIznosSaPdv.textContent = pdv.toFixed(2);
+   inpDodajpdvVrijednost.value = '';
+   plusPdvPostotak.value = '17';
+   return;
+ }
+
   tdSaPdv.textContent = +inpDodajpdvVrijednost.value + +pdv.toFixed(2);
 }
 
